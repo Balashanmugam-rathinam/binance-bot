8b1e8235ca50b3708465979449d916094b241e00
shabalx <146547548+shabalx@users.noreply.github.com>
2026-05-20 16:34:57 +0530

    Fixed Binance testnet initialization

--- bot/client.py
+++ bot/client.py
@@ -1,6 +1,7 @@
 from binance.client import Client
 from dotenv import load_dotenv
 import os
+import streamlit as st
 
 load_dotenv()
 
@@ -8,10 +9,23 @@ load_dotenv()
 class BinanceFuturesClient:
 
     def __init__(self):
-        api_key = os.getenv("BINANCE_API_KEY")
-        api_secret = os.getenv("BINANCE_API_SECRET")
 
-        self.client = Client(api_key, api_secret)
+        api_key = (
+            os.getenv("BINANCE_API_KEY")
+            or st.secrets["BINANCE_API_KEY"]
+        )
+
+        api_secret = (
+            os.getenv("BINANCE_API_SECRET")
+            or st.secrets["BINANCE_API_SECRET"]
+        )
+
+        # IMPORTANT FIX
+        self.client = Client(
+            api_key=api_key,
+            api_secret=api_secret,
+            testnet=True
+        )
 
         # Futures Testnet URL
         self.client.FUTURES_URL = "https://testnet.binancefuture.com/fapi"
